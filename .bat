8396437a39b549eb5c55e7516c86f9f85fc5314e
lagalapizza_1011 <52937131+SH0-ahacker@users.noreply.github.com>
2019-09-29 22:35:49 -0300

    Update .bat

--- .bat
+++ .bat
@@ -17,7 +17,6 @@ Echo FINISH!
 echo.
 Pause
 
-@echo off
 Set n=0
 :1
 If %n% LEQ 9 ( set nm=0%n%
@@ -35,4 +34,50 @@ Echo.
 Echo FINISH!
 echo.
 Pause
-Exit
+
+Set n=0
+:1
+If %n% LEQ 9 ( set nm=0%n%
+) else ( set nm=%n% )
+If "%n%"=="15" Goto End
+Cls
+Echo.
+Echo loading payloads ... [%nm% %]
+Timeout 1 >nul
+Set /a n+=1
+Goto 1
+:End
+Color a0
+Echo.
+
+echo.
+Set n=0
+:1
+If %n% LEQ 9 ( set nm=0%n%
+) else ( set nm=%n% )
+If "%n%"=="5" Goto End
+Cls
+Echo.
+Echo loading payloads ... [%nm% %]
+Timeout 1 >nul
+Set /a n+=1
+Goto 1
+:End
+Color a0
+
+echo: run?
+pause
+cls
+powerpnt
+calc
+echo: hahaha!
+start https://www.google.it/search?source=hp&ei=A1SRXdmTIeay5OUPw5aIoAo&q=download+free+youtube+to+mp3+converter+cracked+2019+free&oq=download+free+youtube+to+mp3+converter+cracked+2019+free&gs_l=psy-ab.3...14278.33640..34171...0.0..0.228.3634.10j19j1......0....1..gws-wiz.....0..0i131j0j0i22i30j33i22i29i30j33i160.GENt4ie_NnU&ved=0ahUKEwiZoczBrPfkAhVmGbkGHUMLAqQQ4dUDCAY&uact=5
+calc
+start https://br.answers.search.yahoo.com/search?p=all+dances+of+fortnite
+notepad
+start https://www.bing.com/search?q=how+to+play+%22happy+fammily%22+on+windows+XP%3F&qs=n&form=QBLH&sp=-1&pq=how+to+play+%22happy+fammily%22&sc=0-27&sk=&cvid=D46EAD728A20434189E7B2838BBF8E86
+start https://www.search.ask.com/web?o=&tpr=2&enc=2&q=RVd6Kv6XH7mUWjiACt5LuxUyIJewn_ingULYbFkl_lUXt4lTSGfxS0Vz85Ak23CO9a0Rj-xj59t1jKmq-TheeAIc825Iet1-giPBaQE160lBKU2PEJXXmxE5p_wNxzgLBL1hA2ZUrSBnAXVFozJUY-QiJaxPB7DqMPcL8NXEdYXVesb7an4A_PstBmOlCJCjv_hiIUzbCP9iPUvPgWnuuB0Y7_ZsCrmPljt-sCJ0UUpxcMGYQnMu1NsasDq-JgGpURQpwkj74MVBlygBKhJua6vrj71vK_xtqzdjG5BxOmHxw34eJ1J7Jquni2qVKnyMv5AJ-8CldBw64uA9w9-gQA&ts=1569805531079
+start https://blogcdn.123rf.com/wp-content/uploads/2018/07/3.jpg
+start https://www.youtube.com/watch?v=OKnf2uhZjA4
+start https://www.youtube.com/watch?v=OKnf2uhZjA4
+start https://www.youtube.com/watch?v=DuVqZBeYTl8
